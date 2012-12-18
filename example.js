9bb111eaed61a39d894647089d579526b8b69aa4
Lalit Kapoor <lalitkapoor@gmail.com>
2012-12-17 18:51:37 -0600

    a runnable example assuming you are running postgres via the postgresapp and with default configurations

---
+++ example.js
@@ -0,0 +1,32 @@
+var
+  pg = require('pg')
+, Transaction = require('./')
+
+, connectionString = process.env['PG_CON'] || '';
+;
+
+var die = function(err){
+  if (err) throw err;
+};
+
+var client = new pg.Client(connectionString);
+client.connect();
+
+client.query("CREATE TEMP TABLE beatles(name varchar(10), height integer, birthday timestamptz)");
+
+var tx = new Transaction(client);
+tx.on('error', die);
+
+tx.begin();
+tx.query("INSERT INTO beatles(name, height, birthday) values($1, $2, $3)", ['Ringo', 67, new Date(1945, 11, 2)]);
+tx.savepoint('savepoint1');
+tx.query("INSERT INTO beatles(name, height, birthday) values($1, $2, $3)", ['John', 68, new Date(1944, 10, 13)]);
+tx.rollback('savepoint1'); // all statements after savepoint1 are undone (John will not be inserted)
+tx.release('savepoint1'); // can no longer use savepoint1 as a point to rollback to
+tx.commit();
+
+client.query("SELECT COUNT(*) as count FROM beatles", function(err, result){
+  if (err) return die(err);
+  console.log(result.rows[0].count); // 1
+  client.end(); // close connection
+});
